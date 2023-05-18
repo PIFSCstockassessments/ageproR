2091416db16e9ac79fb7a09cb203d442b10ed642
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-05-17 14:57:31 -1000

    added cli_keyword_heading to util functions

--- R/utils.R
+++ R/utils.R
@@ -47,3 +47,23 @@ read_inp_numeric_line <- function(inp_con) {
 
   return(assert_numeric_substrings(inp_line))
 }
+
+
+
+#' Command-line interface header for AGEPRO Keyword parameters
+#'
+#' Creates an custom header with double-lines (colored in cyan by default), via
+#' cli library.
+#'
+#' @param keyword Text of the header
+#' @param header_color Text Color, R color, or HTML hexidecimal color.
+#'
+#' @keywords internal
+#'
+cli_keyword_heading <- function(keyword, heading_color = "cyan") {
+  d <- cli_div(theme = list(rule = list(
+    color = heading_color,
+    "line-type" = "double")))
+  cli_rule(keyword)
+  cli_end(d)
+}
