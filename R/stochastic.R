8bffb3aedcebe0e60abc324f48b68a575e167749
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-08-07 13:53:24 -1000

    create_stochastic_table

--- R/stochastic.R
+++ R/stochastic.R
@@ -5,6 +5,16 @@
 #' @description
 #' Generalized Class Structure for Stochastic AGEPRO Keyword parameters.
 #'
+#' @param num_obs_yrs Numbers of years in from first to last year of
+#' projection.
+#' @param num_ages Number of Age classes
+#' @param num_fleets Number of Fleets. Default is 1
+#' @param input_option
+#' @param time_flag Logical flag that enables the stochastic parameter to use
+#' as a time-varying array if TRUE (or 1). Otherwise, FALSE the vector will
+#' cover "all years" of the projection. Default is TRUE.
+#'
+#'
 #' @import cli
 #' @importFrom R6 R6Class
 #' @importFrom jsonlite toJSON
@@ -19,48 +29,68 @@ stochastic <- R6Class(
     .time_varying = NULL,
     .stochastic_datafile = NULL,
     .stochastic_table = NULL,
-    .cv_table = NULL
+    .cv_table = NULL,
+
+    .valid_input_options = c(0,1)
+
+
 
   ), public = list (
 
     #' @description
     #' Initializes the stochastic class
     #'
-    #' @param num_obs_yrs Numbers of years in from first to last year of
-    #' projection.
-    #' @param num_ages Number of Age classes
-    #' @param num_fleets Number of Fleets. Default is 1
-    #'
-    initialize = function(num_obs_yrs, num_ages, num_fleets = 1){
+    initialize = function(num_obs_yrs,
+                          num_ages,
+                          num_fleets = 1,
+                          input_option = 1,
+                          time_flag = TRUE){
 
       #initialize tables
       private$.stochastic_table <- vector("list", 1)
       private$.cv_table <- vector("list", 1)
 
+      self$input_option <- input_option
+
+      if(self$time_varying){
+
+        self$stochastastic_table <-
+          self$create_stochastic_table(num_obs_yrs, num_ages, num_fleets)
+
+        self$cv_table <-
+          self$create_stochastic_table(num_obs_yrs, num_ages, num_fleets)
+      }else{
+        #All Years
+        self$stochastic_table <-
+          self$create_stochastic_table(1, num_ages, num_fleets)
+        self$cv_table <- self$create_stochastic_table(1, num_ages, num_fleets)
+
+
+      }
+
+
+
     },
 
-
-
     #' @description
-    #' Create a Stochastic table
+    #' Creates an stochastic table
     #'
-    #' @param num_obs_yrs Numbers of years in from first to last year of
-    #' projection.
-    #' @param num_ages Number of Age classes
-    #' @param num_fleets Number of Fleets. Default is 1
-    #'
-    setup_stochastic_table(num_obs_yrs, num_ages, num_fleets = 1) {
+    create_stochastic_table = function(num_obs_yrs,
+                                      num_ages,
+                                      num_fleets = 1) {
 
 
       fleet_yrs_rows <- num_obs_yrs * num_fleets
       ages_cols <- num_ages
 
-      self$stochastic_table <- matrix(rep(NA, (fleet_yrs_rows * ages_cols) ) ,
-                                      nrow = fleet_yrs_rows,
-                                      ncol = ages_cols)
-
+      return(matrix(rep(NA, (fleet_yrs_rows * ages_cols) ) ,
+                    nrow = fleet_yrs_rows,
+                    ncol = ages_cols))
 
     }
+
+
+
 
 
 
@@ -72,7 +102,8 @@ stochastic <- R6Class(
       if(missing(value)){
         private$.input_option
       } else {
-        checkmate::assert_logical(value)
+        checkmate::assert_integer(value, lower = 0)
+        checkmate::assert_subset(value, private$.valid_input_options)
         private$.input_option <- value
       }
     },
@@ -91,7 +122,7 @@ stochastic <- R6Class(
     #' @field stochastic_table This is the logic for the average stochastic
     #' AGEPRO keyword parameter's at age (and by fleet if fleets are a
     #' factor).
-    stocastic_table = function(value) {
+    stochastic_table = function(value) {
       if(missing(value)){
         private$.stochastic_table
       } else {
