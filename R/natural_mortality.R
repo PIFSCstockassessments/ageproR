9ca77032c28d122c5d40a6d5d55f1d795d11ba55
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-08-08 16:45:55 -1000

    inital natural_mortality

---
+++ R/natural_mortality.R
@@ -0,0 +1,51 @@
+
+#' Input information for the natural mortality rate (M) at Age
+#'
+#' @description
+#' Generalized Class Structure for Natural Mortality rate of age
+#' AGEPRO Keyword parameter.
+#'
+#' @import cli
+#' @importFrom R6 R6Class
+#' @importFrom jsonlite toJSON
+#'
+natural_mortality <- R6Class(
+  "natural_mortality",
+  inherit = stochastic,
+  private = list(
+
+  ),
+  public = list(
+
+    #' @description
+    #' Initializes the stochastic class
+    #'
+    #' @param num_projection_years Numbers of years in from first to last
+    #' year of projection.
+    #' @param num_ages Number of Age classes
+    #' @param input_option Option to indicate Natural Mortality at age will be:
+    #' \itemize{
+    #'  \item `0` By default, done interactively via interface.
+    #'  \item `1` Imported from a the location of an existing data file
+    #' }
+    #' @param time_varying Logical flag that enables  Natural Mortality at age
+    #' to use as a time-varying array if TRUE (or 1). Otherwise, FALSE the
+    #' vector will cover "all years" of the projection. Default is TRUE.
+    #'
+    #'
+    initalize = function(num_projection_years,
+                         num_ages,
+                         input_option = 0,
+                         time_varying = TRUE) {
+
+
+      super$initalize(num_projection_years,
+                      num_ages,
+                      1, #Single, non-Fleet dependent parameter.
+                      input_option,
+                      time_varying)
+    }
+
+  )
+
+)
