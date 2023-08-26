b08b512bc58f5b30fe0990a530706ebbf9977c8d
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2023-08-25 16:53:58 -1000

    un-rollback changes

--- man-roxygen/stochastic_years_ages.R
+++ man-roxygen/stochastic_years_ages.R
@@ -1,16 +1,14 @@
-
-#' @param num_projection_years Numbers of years in from first to last
-#' year of projection.
+#' @param proj_years [Projection years][ageproR::projection_years]:
+#' Input can be Sequence of years in from first to last year of
+#' projection or the number of years in the time projection.
 #' @param num_ages Number of Age classes
-#' @param input_option Option to indicate the stochastic Parameter at age will
-#' be:
+#' @param input_option Option to indicate this parameter will be:
 #' \itemize{
 #'  \item `0` By default, done interactively via interface.
-#'  \item `1` Imported from a the location of an existing data file
+#'  \item `1` Imported via location of an existing data file.
 #' }
-#' @param time_varying Logical flag that enables the stochastic parameter at
-#' age to use as a time-varying array if TRUE (or 1). Otherwise, FALSE the
+#' @param time_varying Logical flag that enables the stochastic parameter
+#' to use as a time-varying array if TRUE (or 1). Otherwise, FALSE the
 #' vector will cover "all years" of the projection. Default is TRUE.
 #'
-#'
-#'
+
