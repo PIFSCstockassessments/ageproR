4c0d825c2d8f73340bf6ff9d8a29ba919a007589
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-01-17 10:31:25 -1000

    added proj_years roxygen template

---
+++ man-roxygen/proj_years.R
@@ -0,0 +1,3 @@
+#' @param proj_years [Projection years][ageproR::projection_years]:
+#' Input can be Sequence of years in from first to last year of
+#' projection or the number of years in the time projection.
