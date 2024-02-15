8e4e2958583123358a75e126cd79e2bc72b22663
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-02-15 09:54:11 -1000

    impelement read_inp_lines

--- R/mortality_fraction.R
+++ R/mortality_fraction.R
@@ -8,6 +8,8 @@
 #' prior to spawning season
 #'
 #' @template proj_years_vector
+#' @template inp_con
+#' @template nline
 #'
 #' @param time_varying
 #' Logical flag that enables the stochastic parameter
@@ -55,7 +57,8 @@ mortality_fraction_prior_spawn <- R6Class(
 
     # Creates matrix object vector for natural_mortality_before_spawn and
     # fishing_mortality_before_spawn
-    set_fraction_mortality_matrix = function(time_varying,
+    set_fraction_mortality_matrix = function(value,
+                                             time_varying,
                                              row_names = NULL) {
 
       default_proportion <- 0.5
@@ -90,7 +93,12 @@ mortality_fraction_prior_spawn <- R6Class(
     #' @description
     #' Initializes the class
     #'
-    initialize = function(proj_years_vector, time_varying = FALSE) {
+    #' @param default_proportion
+    #' Proportion default values. Time varying will adjust the values by
+    #' number of projection years.
+    #'
+    initialize = function(proj_years_vector, time_varying = FALSE,
+                          default_proportion = 0.5) {
 
       checkmate::assert_logical(time_varying,
                                 any.missing = FALSE, all.missing=FALSE,
@@ -100,15 +108,85 @@ mortality_fraction_prior_spawn <- R6Class(
       private$set_projection_years(proj_years_vector)
       private$.time_varying <- time_varying
 
+      #zfrac_default: defaults
+      if(private$.time_varying) {
+        default_proportion <-
+          rep(default_proportion/private$.projection_years$count,
+              private$.projection_years$count)
+      }
+
       private$.natural_mortality_prior_spawn <-
-        private$set_fraction_mortality_matrix(private$.time_varying,
-                                              row_names = "natural_mortality_prior_spawn")
+        private$set_fraction_mortality_matrix(default_proportion,
+                                              private$.time_varying,
+                                              "natural_mortality_prior_spawn")
 
       private$.fishing_mortality_prior_spawn <-
-        private$set_fraction_mortality_matrix(private$.time_varying,
-                                              row_names = "fishing_mortality_prior_spawn")
+        private$set_fraction_mortality_matrix(default_proportion,
+                                              private$.time_varying,
+                                              "fishing_mortality_prior_spawn")
+
+    },
+
+
+    #' @description
+    #' Reads in the values from the keyword parameter BIOLOGICAL from the
+    #' AGEPRO Input file
+    #'
+    read_inp_lines = function(inp_con,
+                              nline,
+                              proj_years_vector) {
+
+
+      private$set_projection_years(proj_years_vector)
+
+      cli::cli_alert_info("Reading {.strong {private$.keyword_name}}")
+
+      nline <- nline + 1
+      inp_line <- read_inp_numeric_line(inp_con)
+
+      private$set_time_varying(inp_line)
+      cli::cli_alert(paste0("Line {nline}: ",
+                            "time_varying: {.var {private$.time_varying}} ",
+                            "{.emph {as.logical(private$.time_varying)}}"))
+
+      if(isTRUE(private$.time_varying)){
+        names_proj_years_count <- paste0(private$.projection_years$count,
+                                         " years")
+      }else{
+        names_proj_years_count <- "All Years"
+      }
+
+
+      nline <- nline + 1
+      tf_inp_line <- read_inp_numeric_line(inp_con)
+
+      cli::cli_alert(paste0("Line {nline}: ",
+                            "fishing_mortality_prior_spawn: ",
+                            "{.val {tf_inp_line}} ",
+                            "{.emph ({names_proj_years_count})}" ))
+
+      private$.fishing_mortality_prior_spawn <-
+        private$set_fraction_mortality_matrix(
+          tf_inp_line,
+          private$.time_varying,
+          row_names = "fishing_mortality_prior_spawn")
+
+
+      nline <- nline + 1
+      tm_inp_line <- read_inp_numeric_line(inp_con)
+      cli::cli_alert(paste0("Line {nline}: ",
+                            "natural_mortality_prior_spawn: ",
+                            "{.val {tm_inp_line}} ",
+                            "{.emph ({names_proj_years_count})}" ))
+
+      private$.natural_mortality_prior_spawn <-
+        private$set_fraction_mortality_matrix(
+          tm_inp_line,
+          private$.time_varying,
+          row_names = "natural_mortality_prior_spawn")
 
     }
+
 
   ),
   active = list(
@@ -120,7 +198,7 @@ mortality_fraction_prior_spawn <- R6Class(
       if(isFALSE(missing(value))){
         stop("active binding is read only", call. = FALSE)
       }
-      private$.time_varying
+      cli::cli_text("{{private$.time_varying}} {.emph({as.logical(private$.time_varying)} )}")
     },
 
     #' @field proportion_total_mortality
