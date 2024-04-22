b3fcb09a84f7c41241b60473f49ddf9daa1e8563
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-04-22 12:57:25 -1000

    Error message tweaks

--- R/agepro_model.R
+++ R/agepro_model.R
@@ -579,7 +579,7 @@ agepro_model <- R6Class(
         },
         error = function(err) {
 
-          message(paste0("Error: \n", conditionMessage(err) ))
+          message(paste0("Error: \n", gsub("\\.$","",conditionMessage(err)) ))
         })
       }
     },
