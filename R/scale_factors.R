8e70471d7dc74fa529224d45f6c9f909a7a4f8ad
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-06-27 14:21:11 -1000

    inital scale factors commit

---
+++ R/scale_factors.R
@@ -0,0 +1,24 @@
+
+
+#' @title Scale Factors
+#'
+#' @description
+#' Input information on scaling factors for biomass, recruitment, and stock size
+#'
+#' @include optional_options_flags.R
+#'
+#' @export
+#' @importFrom R6 R6Class
+#'
+scale_factors <-R6Class(
+  "scale_factors",
+  public = list(
+
+  ),
+  active = list(
+
+  ),
+  private = list(
+
+  )
+)
