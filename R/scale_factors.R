439923391b5fd914181508474117658263df9609
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-07-01 16:07:15 -1000

    scale_factors initialization

--- R/scale_factors.R
+++ R/scale_factors.R
@@ -14,6 +14,44 @@ scale_factors <-R6Class(
   "scale_factors",
   public = list(
 
+    #' @field flag
+    #' R6class containing option_flags
+    flag = options_flags$new(),
+
+    #' @description
+    #' Initializes the class
+    #'
+    #' @param scale_bio
+    #' Output units of biomass express in thousand metric units
+    #'
+    #' @param scale_recruit
+    #' Output Units of Recruitment Numbers
+    #'
+    #' @param scale_stock_size
+    #' Output Units of Stock Size Numbers
+    #'
+    initialize = function(scale_bio = 0,
+                          scale_recruit = 0,
+                          scale_stock_size = 0) {
+
+      #Check for defaults
+      if(all(c(all.equal(scale_bio, 0),
+               all.equal(scale_recruit, 0),
+               all.equal(scale_stock_size, 0)))) {
+        cli::cli_alert(paste0("Default values set, ",
+                              "options_flag enable_scale_factors to FALSE"))
+        suppressMessages(private$set_enable_scale_factors(FALSE))
+      } else{
+        cli::cli_alert(paste0("Values for reference_points set. ",
+                              "Enable options_flag enable_scale_factors ",
+                              "as TRUE"))
+        private$set_enable_scale_factors(TRUE)
+        #self$print()
+      }
+    }
+
+
+
   ),
   active = list(
 
@@ -61,7 +99,32 @@ scale_factors <-R6Class(
 
     .biomass_scale = NULL,
     .recruitment_scale = NULL,
-    .stock_size_scale = NULL
+    .stock_size_scale = NULL,
+
+    # Wrapper Function to toggle enable_reference_points options_flag.
+    set_enable_scale_factors = function(x) {
+
+      checkmate::assert_logical(x, null.ok = TRUE)
+
+      #Set value to options flags field reference "flag"
+      self$flag$op$enable_scale_factors <- x
+
+      cli::cli_alert(
+        paste0("enable_scale_factors : ",
+               "{.val ",
+               "{self$flag$op$enable_scale_factors}}"))
+
+
+    },
+
+    # Error message when setting values to reference_points while
+    # enable_reference_points is FALSE
+    unenabled_options_flag_message = function() {
+      return(invisible(
+        paste0("enable_reference_points is FALSE. ",
+               "Set flag to TRUE to set value.")
+      ))
+    }
 
 
   )
