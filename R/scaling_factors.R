1572bd01a012785ca4311d65e08cf893ccb1e5df
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2024-07-09 14:30:18 -1000

    fixup typo that prevents resetting enable_scaling_factors during reinitialization

--- R/scaling_factors.R
+++ R/scaling_factors.R
@@ -277,7 +277,7 @@ scaling_factors <-R6Class(
     #Reset option_flag to NULL at initialization
     reset_options_flags = function() {
 
-      if(isFALSE(is.null(self$flag$op$enable_scale_options))){
+      if(isFALSE(is.null(self$flag$op$enable_scaling_options))){
         cli::cli_alert("Reset enable_scaling_factors to NULL for initalization")
         self$flag$op$enable_scale_options <- NULL
       }
